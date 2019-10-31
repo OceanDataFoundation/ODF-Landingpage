74b61ae499364945a89c75ee739c7a0fef388290
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-10-31 15:51:21 +0100

    feat: Add space array

---
+++ src/utils/configs/confSpace.js
@@ -0,0 +1,22 @@
+// ----------------------------------------------------------------------------
+//
+// Description: Config, padding & margin spacing
+//
+// The distance is based on using 4 as the base-line unit.
+// Unit in pixels are: 0, 2, 4, 8, 16, 24, 32, 40, 48, 56, 64
+// ============================================================================
+//
+
+export const space = [
+  '0',
+  '0.125rem',
+  '0.25rem',
+  '0.5rem',
+  '1rem',
+  '1.5rem',
+  '2rem',
+  '2.5rem',
+  '3rem',
+  '3.5rem',
+  '4rem',
+]
