a12ef1741970b382f19d7b0c301245e9dd44b654
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-10-31 15:52:16 +0100

    feat: Add design tokens

---
+++ src/utils/tokens/tokenBorderRadius.js
@@ -0,0 +1,16 @@
+// ----------------------------------------------------------------------------
+//
+// Description: Token, border-radius
+// ============================================================================
+//
+
+export const borderRadius = {
+  BORDER_RADIUS_NONE: '0',
+  BORDER_RADIUS_1: '2px',
+  BORDER_RADIUS_2: '4px',
+  BORDER_RADIUS_3: '8px',
+  BORDER_RADIUS_4: '16px',
+  BORDER_RADIUS_5: '32px',
+  BORDER_RADIUS_6: '9999px',
+  BORDER_RADIUS_7: '100%',
+}
---
+++ src/utils/tokens/tokenColorBrand.js
@@ -0,0 +1,17 @@
+// ----------------------------------------------------------------------------
+//
+// Description: Token, neutral brand palette
+// ============================================================================
+//
+
+export const colorBrandGreen = {
+  GREEN_TINT_90: '#',
+  GREEN_TINT_80: '#',
+  GREEN_TINT_70: '#',
+  GREEN_TINT_60: '#',
+  GREEN_TINT_50: '#',
+  GREEN_TINT_40: '#',
+  GREEN_TINT_30: '#',
+  GREEN_TINT_20: '#',
+  GREEN_TINT_10: '#',
+}
