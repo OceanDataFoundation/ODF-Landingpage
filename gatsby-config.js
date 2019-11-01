79e0367e0a97c9d6b042ccf27c2e85989dc2e0e0
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-11-01 13:55:24 +0100

    chore: Update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Ocean Data Foundation`,
     author: `Kyle Mathews`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
