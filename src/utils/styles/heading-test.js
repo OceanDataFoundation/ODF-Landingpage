02bb45fa02aaf0865ed2e1c03e2fff7d0e9aa5a6
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-11-04 15:24:18 +0100

    feat: Add token

--- src/utils/styles/heading-test.js
+++ src/utils/styles/heading-test.js
@@ -1,10 +1,13 @@
 import { css } from 'styled-components'
 
-// Token H1
+// Token
+import { fontWeight } from '../../utils/tokens/tokenFontWeight'
 
+// Token H1
 export const Heading1 = css`
   h1 {
     color: yellow;
+    font-weight: ${fontWeight.FONT_WEIGHT_5};
     background-color: black;
   }
 `
