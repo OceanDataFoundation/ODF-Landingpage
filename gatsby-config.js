de09e006d1e65a7427cebd58732a134bfe429f0a
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-11-06 14:46:32 +0100

    chore: Add styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,12 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: contentfulConfig,
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
