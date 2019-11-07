78ca6c8e518d00d768a4bc98bcd14ca4650801ae
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-11-07 09:36:30 +0100

    feat: Remove old css

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import { GlobalStyle } from '../utils/styles/global-style'
+
+import Header from './header'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,6 +26,8 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <GlobalStyle />
+
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
@@ -32,8 +35,7 @@ const Layout = ({ children }) => {
           maxWidth: 960,
           padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
-        }}
-      >
+        }}>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
