724d0582eadd9eaa09c48353233c436cda9d01ad
Knut Sorknes <knut.sorknes@makingwaves.com>
2019-11-08 14:02:49 +0100

    feat: New container component

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+// Tokens
+import { breakpoints } from '../../utils/tokens/tokenBreakpoints'
+
+export const Container = styled.div`
+  max-width: ${breakpoints.BREAKPOINT_4};
+
+  background-color: blue;
+`
