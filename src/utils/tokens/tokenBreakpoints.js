0c566cfa27faaeb934bc45b8fbdce0fd79d38326
Knut Sorknes <knut.sorknes@gmail.com>
2019-11-11 15:47:00 +0100

    feat: Change breakpoints

--- src/utils/tokens/tokenBreakpoints.js
+++ src/utils/tokens/tokenBreakpoints.js
@@ -6,9 +6,9 @@
 
 export const breakpoints = {
   BREAKPOINT_1: '480px',
-  BREAKPOINT_2: '736px',
-  BREAKPOINT_3: '980px',
+  BREAKPOINT_2: '768px',
+  BREAKPOINT_3: '1024px',
   BREAKPOINT_4: '1280px',
-  BREAKPOINT_5: '1690px',
-  BREAKPOINT_6: '1950px',
+  BREAKPOINT_5: '1366px',
+  BREAKPOINT_6: '1920px',
 }
