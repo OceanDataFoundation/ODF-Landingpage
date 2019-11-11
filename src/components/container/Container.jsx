d8e879a30eaadadd90b355250a77e0b554f69284
Knut Sorknes <knut.sorknes@gmail.com>
2019-11-11 13:25:54 +0100

    feat: Add grid and fluid option

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -8,5 +8,8 @@ import { space } from '../../utils/configs/confSpace'
 
 export const Container = styled.div`
   margin: ${space[0]} auto;
+  padding-right: ${props => (props.fluid ? 0 : space[3])};
+  padding-left: ${props => (props.fluid ? 0 : space[3])};
+  display: grid;
   max-width: ${props => (props.fluid ? `100%` : breakpoints.BREAKPOINT_4)};
 `
