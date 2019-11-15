22548ed424b9c0c51941302de41a4b8e2bc707c5
Knut Sorknes <knut.sorknes@gmail.com>
2019-11-15 16:28:31 +0100

    feat: Work on news page

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/site-layout/Layout'
 import SEO from '../components/seo/seo'
+
+// Components
+import { Container } from '../components/container/Container'
+
+import { H2 } from '../components/typography/heading/Heading'
+import P from '../components/typography/paragraph/Paragraph'
 
 const NewsPosts = ({ data }) => {
   const newsPosts = data.allContentfulNews.edges
@@ -10,16 +17,28 @@ const NewsPosts = ({ data }) => {
   return (
     <Layout>
       <SEO title="News posts" />
-      <h1>{"Here's a list of all blogposts!"}</h1>
-      <div className="">
+      <Container
+        style={{
+          gridTemplateColumns: 'repeat(2, 1fr)',
+          gridTemplateRows: 'repeat(8, 15vw)',
+          gridColumnGap: '32px',
+        }}>
         {newsPosts.map(({ node: post }) => (
-          <div key={post.id}>
-            <Link to={`news/${post.slug}`}>{post.title}</Link>
+          <div key={post.id} style={{ backgroundColor: 'grey' }}>
+            <H2>{post.title}</H2>
+            {post.image ? (
+              <Img
+                fluid={post.image.fluid}
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+            ) : null}
+            <P>{post.excerpt}</P>
+            <Link to={`/news/${post.slug}`}>{post.title}</Link>
           </div>
         ))}
-        <span className="" />
+
         <Link to="/">Go back to the homepage</Link>
-      </div>
+      </Container>
     </Layout>
   )
 }
