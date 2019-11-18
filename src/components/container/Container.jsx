ec44a1d2d2a3563db87aeec2531d1c04a483ffab
Knut Sorknes <knut.sorknes@gmail.com>
2019-11-18 19:44:23 +0100

    feat: Add new props; col & offset

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -10,23 +10,31 @@ import { space } from '../../utils/configs/confSpace'
 import { mediaQuery } from '../../utils/mixins/mixMediaQuery'
 
 export const Container = styled.section`
-  margin: ${space[0]} auto;
+  max-width: ${props => (props.fluid ? `100%` : breakpoints.BREAKPOINT_4)};
+  margin-top: ${props => (props.offset ? '80px' : '0')};
+  margin-right: auto;
+  margin-bottom: ${space[0]};
+  margin-left: auto;
   padding-right: ${props => (props.fluid ? 0 : space[4])};
   padding-left: ${props => (props.fluid ? 0 : space[4])};
   display: grid;
-  max-width: ${props => (props.fluid ? `100%` : breakpoints.BREAKPOINT_4)};
-
   grid-template-columns: ${props =>
-    props.twoCol ? `repeat(2, 1fr)` : `repeat(1, 1fr)`};
-  gap: ${props => (props.twoCol ? `32px` : null)};
+    props.col === '3'
+      ? `repeat(3, 1fr)`
+      : props.col === '2'
+      ? `repeat(2, 1fr)`
+      : `repeat(1, 1fr)`};
+  gap: ${props => (props.col === '3' || '2' ? `32px` : null)};
 
   ${mediaQuery.BREAKPOINT_2`
     padding-right: ${props => (props.fluid ? 0 : space[6])};
     padding-left: ${props => (props.fluid ? 0 : space[6])};
+    margin-top: ${props => (props.offset ? '100px' : '0')};
   `};
 
   ${mediaQuery.BREAKPOINT_3`
     padding-right: ${props => (props.fluid ? 0 : space[8])};
     padding-left: ${props => (props.fluid ? 0 : space[8])};
+    margin-top: ${props => (props.offset ? '120px' : '0')};
   `};
 `
