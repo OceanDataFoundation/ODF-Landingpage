31b94c8defccb3b1f6c72efcdd143aab33254009
Knut Sorknes <knut.sorknes@gmail.com>
2019-11-18 16:17:29 +0100

    Add two col prop

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -16,6 +16,10 @@ export const Container = styled.section`
   display: grid;
   max-width: ${props => (props.fluid ? `100%` : breakpoints.BREAKPOINT_4)};
 
+  grid-template-columns: ${props =>
+    props.twoCol ? `repeat(2, 1fr)` : `repeat(1, 1fr)`};
+  gap: ${props => (props.twoCol ? `32px` : null)};
+
   ${mediaQuery.BREAKPOINT_2`
     padding-right: ${props => (props.fluid ? 0 : space[6])};
     padding-left: ${props => (props.fluid ? 0 : space[6])};
