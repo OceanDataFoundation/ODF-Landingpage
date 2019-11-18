7584af97fe3ce2d4524a600b293c7e2876a87ab0
Knut Sorknes <knut.sorknes@gmail.com>
2019-11-18 16:17:16 +0100

    feat: Add small component

---
+++ src/components/typography/small/Small.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+// Config
+import { typeScale } from '../../../utils/configs/confTypeScale'
+
+export const Small = styled.small`
+  font-size: ${typeScale.TEXT_PRESET_1};
+`
