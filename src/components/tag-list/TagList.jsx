47529d415c353503b944f741d1e69bacd484bc6e
Knut Sorknes <knut.sorknes@gmail.com>
2019-11-25 15:10:36 +0100

    feat: Add tag-list component

---
+++ src/components/tag-list/TagList.jsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+
+// Tokens
+import { borderWidth } from '../../utils/tokens/tokenBorderWidth'
+import { borderRadius } from '../../utils/tokens/tokenBorderRadius'
+import { colorNeutral } from '../../utils/tokens/tokenColorNeutral'
+import { colorBrandGreen } from '../../utils/tokens/tokenColorBrand'
+import { fontWeight } from '../../utils/tokens/tokenFontWeight'
+
+// Config
+import { space } from '../../utils/configs/confSpace'
+
+// Mixins
+import { mediaQuery } from '../../utils/mixins/mixMediaQuery'
+
+export const TagList = styled.ol`
+  padding: ${space[4]} ${space[0]} ${space[0]} ${space[6]};
+  margin: ${space[6]} ${space[0]} ${space[10]} ${space[0]};
+  list-style-type: none;
+  display: flex;
+  position: relative;
+
+  ${mediaQuery.BREAKPOINT_3`
+    padding: ${space[4]} ${space[0]};
+  `};
+
+  ::before {
+    margin-right: ${space[4]};
+    font-weight: ${fontWeight.FONT_WEIGHT_4};
+    content: 'Tags:';
+    position: relative;
+    top: 4px;
+  }
+
+  ::after {
+    width: 16px;
+    height: 4px;
+    margin-top: 5px;
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 0px;
+    background-color: ${colorBrandGreen.GREEN_TINT_50};
+
+    ${mediaQuery.BREAKPOINT_3`
+      margin-top: -2px;
+      left: -26px;
+    `};
+  }
+
+  li {
+    padding: ${space[2]} ${space[4]};
+    border-radius: ${borderRadius.BORDER_RADIUS_2};
+    background-color: ${colorNeutral.NEUTRAL_TINT_95};
+
+    :not(:last-child) {
+      margin-right: ${space[4]};
+    }
+  }
+`
