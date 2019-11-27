a53ab29112818f6dfaff0084f2ff0ee21d3b5a2d
Hans-Jakob <hjbrandt@me.com>
2019-11-27 13:27:23 +0100

    feature: Submit forms to netlify (test)

--- src/components/contact-form/ContactForm.jsx
+++ src/components/contact-form/ContactForm.jsx
@@ -53,17 +53,32 @@ const encode = () => data => {
 const ContactForm = () => {
   const [state, setState] = useState({})
 
-  const handleChange = event => {
-    setState({ ...state, [event.target.name]: event.target.value })
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
   }
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    const form = event.target
-    axios
-      .post('/', {
-        body: encode({ 'form-name': form.getAttribute('name'), ...state }),
-      })
+  // const handleSubmit = event => {
+  //   event.preventDefault()
+  //   const form = event.target
+  //   axios
+  //     .post('/', {
+  //       body: encode({ 'form-name': form.getAttribute('name'), ...state }),
+  //     })
+  //     .then(() => navigate(form.getAttribute('action')))
+  //     .catch(error => alert(error))
+  // }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
       .then(() => navigate(form.getAttribute('action')))
       .catch(error => alert(error))
   }
@@ -76,7 +91,14 @@ const ContactForm = () => {
       data-netlify="true"
       data-netlify-honeypot="bot-field"
       onSubmit={handleSubmit}>
+      {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
       <input type="hidden" name="form-name" value="contact" />
+      <p hidden>
+        <label>
+          Don’t fill this out:{' '}
+          <input name="bot-field" onChange={handleChange} />
+        </label>
+      </p>
 
       <label>
         <input
