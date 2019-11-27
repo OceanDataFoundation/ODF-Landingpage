e5586c5bd7f602632689d1d041528e3bc18aff14
Hans-Jakob <hjbrandt@me.com>
2019-11-27 14:08:43 +0100

    style: Remove unused code

--- src/components/contact-form/ContactForm.jsx
+++ src/components/contact-form/ContactForm.jsx
@@ -52,48 +52,13 @@ const encode = () => data => {
 }
 
 const ContactForm = () => {
-  // const [state, setState] = useState({})
-
-  // const handleChange = e => {
-  //   setState({ ...state, [e.target.name]: e.target.value })
-  // }
-
-  // const handleSubmit = event => {
-  //   event.preventDefault()
-  //   const form = event.target
-  //   axios
-  //     .post('/', {
-  //       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //       body: encode({ 'form-name': form.getAttribute('name'), ...state }),
-  //     })
-  //     .then(() => navigate(form.getAttribute('action')))
-  //     .catch(error => alert(error))
-  // }
-
-  // const handleSubmit = e => {
-  //   e.preventDefault()
-  //   const form = e.target
-  //   fetch('/', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //     body: encode({
-  //       'form-name': form.getAttribute('name'),
-  //       ...state,
-  //     }),
-  //   })
-  //     .then(() => navigate(form.getAttribute('action')))
-  //     .catch(error => alert(error))
-  // }
-
   return (
     <Form
       name="contact"
       method="post"
       action="/contact/success/"
       data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      // onSubmit={handleSubmit}
-    >
+      data-netlify-honeypot="bot-field">
       {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
       <input type="hidden" name="form-name" value="contact" />
       <p hidden>
@@ -103,39 +68,19 @@ const ContactForm = () => {
       </p>
 
       <label>
-        <input
-          placeholder="Name"
-          type="text"
-          name="name"
-          // onChange={handleChange}
-        />
+        <input placeholder="Name" type="text" name="name" />
       </label>
 
       <label>
-        <input
-          placeholder="Email Address"
-          type="email"
-          name="email"
-          // onChange={handleChange}
-        />
+        <input placeholder="Email Address" type="email" name="email" />
       </label>
 
       <label>
-        <input
-          placeholder="Subject"
-          type="text"
-          name="subject"
-          // onChange={handleChange}
-        />
+        <input placeholder="Subject" type="text" name="subject" />
       </label>
 
       <label>
-        <textarea
-          placeholder="Message"
-          name="message"
-          // onChange={handleChange}
-          rows="10"
-        />
+        <textarea placeholder="Message" name="message" rows="10" />
       </label>
 
       <button type="submit">Send</button>
