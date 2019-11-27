d1d8d2f5e035cb7bbdb88e7c747292ee841927bd
Hans-Jakob <hjbrandt@me.com>
2019-11-27 13:46:24 +0100

    refactor: Second try

--- src/components/contact-form/ContactForm.jsx
+++ src/components/contact-form/ContactForm.jsx
@@ -44,6 +44,7 @@ const Form = styled.form`
   }
 `
 
+// Component helper function
 const encode = () => data => {
   return Object.keys(data)
     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
@@ -57,31 +58,32 @@ const ContactForm = () => {
     setState({ ...state, [e.target.name]: e.target.value })
   }
 
-  // const handleSubmit = event => {
-  //   event.preventDefault()
-  //   const form = event.target
-  //   axios
-  //     .post('/', {
-  //       body: encode({ 'form-name': form.getAttribute('name'), ...state }),
-  //     })
-  //     .then(() => navigate(form.getAttribute('action')))
-  //     .catch(error => alert(error))
-  // }
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...state,
-      }),
-    })
+  const handleSubmit = event => {
+    event.preventDefault()
+    const form = event.target
+    axios
+      .post('/', {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({ 'form-name': form.getAttribute('name'), ...state }),
+      })
       .then(() => navigate(form.getAttribute('action')))
       .catch(error => alert(error))
   }
+
+  // const handleSubmit = e => {
+  //   e.preventDefault()
+  //   const form = e.target
+  //   fetch('/', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  //     body: encode({
+  //       'form-name': form.getAttribute('name'),
+  //       ...state,
+  //     }),
+  //   })
+  //     .then(() => navigate(form.getAttribute('action')))
+  //     .catch(error => alert(error))
+  // }
 
   return (
     <Form
