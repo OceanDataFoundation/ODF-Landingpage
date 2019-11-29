419a4fa1f453f9d69bd3656851637f00b74b7dd7
Knut Sorknes <knut.sorknes@gmail.com>
2019-11-29 12:27:07 +0100

    fix: Theme clor

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,8 +48,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0F161B`,
+        theme_color: `#0F161B`,
         display: `minimal-ui`,
         icon: `src/images/odf-icon.png`, // This path is relative to the root of the site.
       },
