45a1cb62bcea556b3398aef6468c0d57d09d89bd
Knut Sorknes <knut.sorknes@gmail.com>
2019-12-04 20:41:19 +0100

    feat: Add pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const path = require(`path`)
 const slash = require(`slash`)
+const _ = require('lodash')
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -45,5 +46,70 @@ exports.createPages = async ({ graphql, actions }) => {
         next,
       },
     })
+  })
+
+  // Create news posts list
+  // ----------------------------------------------------------------------------
+  const PAGE_SIZE = 1
+  const newsListTemplate = path.resolve(`./src/templates/news-list.js`)
+  const newsPostTemplateTest = path.resolve(
+    `./src/templates/news-post-list-test.js`
+  )
+
+  return new Promise((resolve, reject) => {
+    resolve(
+      graphql(
+        `
+          query {
+            posts: allContentfulTest(
+              sort: { order: DESC, fields: [createdAt] }
+            ) {
+              edges {
+                node {
+                  slug
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+
+        // Break the entries into chunks according to
+        let chunks = _.chunk(result.data.posts.edges, PAGE_SIZE)
+
+        // For each of the chunks, call createPage()
+        chunks.forEach((chunk, index) => {
+          createPage({
+            path: `blog/page/${index + 1}`,
+            component: newsListTemplate,
+            context: {
+              skip: PAGE_SIZE * index,
+              limit: PAGE_SIZE,
+              pageNumber: index + 1,
+              hasNextPage: index != chunks.length - 1,
+              nextPageLink: `/blog/page/${index + 2}`,
+
+              hasPrevPage: index != chunks.length + 1,
+              prevPageLink: `/blog/page/${index}`,
+            },
+          })
+        })
+
+        // Create a blog page
+        result.data.posts.edges.forEach(({ node }) => {
+          // loop over split pages
+          createPage({
+            path: `blog/${node.slug}`,
+            component: newsPostTemplateTest,
+            context: {
+              slug: node.slug,
+            },
+          })
+        })
+      })
+    )
   })
 }
