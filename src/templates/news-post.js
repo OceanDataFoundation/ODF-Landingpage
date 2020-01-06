82fd56791177c4555d3fc86098a456c306a589f1
Knut Sorknes <knut.sorknes@gmail.com>
2020-01-06 10:57:23 +0100

    feat: Add og:image

--- src/templates/news-post.js
+++ src/templates/news-post.js
@@ -36,7 +36,7 @@ const NewsPage = ({ data }) => {
 
   return (
     <Layout>
-      <SEO title={title} metaDescription={excerpt} />
+      <SEO title={title} metaDescription={excerpt} image={image.file.url} />
 
       <Container offset="true">
         <Article>
@@ -99,6 +99,9 @@ export const pageQuery = graphql`
       image {
         fluid(maxWidth: 1200, quality: 80) {
           ...GatsbyContentfulFluid
+        }
+        file {
+          url
         }
       }
       imageCaption {
