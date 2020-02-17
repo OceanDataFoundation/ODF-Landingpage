a96c1a8063703a2e27c981b86adc5e6537d27d06
Hans-Jakob Brandt <34381881+hjbrandt@users.noreply.github.com>
2020-02-17 15:53:57 +0100

    feat: Add blog list and blog post (WIP)

---
+++ src/templates/blog-post.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+import Layout from '../components/site-layout/Layout'
+import SEO from '../components/seo/seo'
+
+// Components
+import { Container } from '../components/container/Container'
+import {
+  Article,
+  ArticleContainer,
+  ArticleContent,
+} from '../components/article/Article'
+import { Header } from '../components/header/Header'
+import { Meta } from '../components/meta/Meta'
+import { Figure } from '../components/figure/Figure'
+import { Figcaption } from '../components/figcaption/Figcaption'
+import { TagList } from '../components/tag-list/TagList'
+import LinkButton from '../components/link-button/LinkButton'
+
+import { H1 } from '../components/typography/heading/Heading'
+import P from '../components/typography/paragraph/Paragraph'
+
+const BlogPost = ({ data }) => {
+  console.log('TCL: BlogPost -> data', data)
+  const { title, excerpt, coverImage, coverCaption, keywords } = data.blogPost
+
+  return (
+    <Layout>
+      <SEO
+        title={title}
+        metaDescription={excerpt}
+        image={`https:${coverImage.file.url}`}
+      />
+
+      <Container offset="true">
+        <Article>
+          <Header>
+            <Meta>
+              {/* {createdAt}&nbsp;&nbsp;|&nbsp;&nbsp;By: {author && author} */}
+            </Meta>
+            <H1>{title && title}</H1>
+            <P lead>{excerpt && excerpt}</P>
+          </Header>
+
+          {coverImage && (
+            <Figure>
+              <Img fluid={coverImage.fluid} style={{ maxHeight: '600px' }} />
+              {coverCaption && (
+                <Figcaption as="figcaption">
+                  {coverCaption.coverCaption && coverCaption.coverCaption}
+                </Figcaption>
+              )}
+            </Figure>
+          )}
+
+          <ArticleContainer>
+            <ArticleContent>
+              {/* TODO: Add content */}
+              {keywords && (
+                <TagList>
+                  {keywords.map((tag, index) => (
+                    <li key={index}>{tag}</li>
+                  ))}
+                </TagList>
+              )}
+            </ArticleContent>
+          </ArticleContainer>
+        </Article>
+
+        <LinkButton to="/blog/page/1" showArrow alignCenter>
+          See all posts
+        </LinkButton>
+      </Container>
+    </Layout>
+  )
+}
+
+export default BlogPost
+
+export const pageQuery = graphql`
+  query BlogQuery($slug: String) {
+    blogPost: contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      slug
+      createdAt(formatString: "MMMM DD, YYYY")
+      teaser
+      # author
+      coverImage {
+        fluid(maxWidth: 1200, quality: 80) {
+          ...GatsbyContentfulFluid
+        }
+        file {
+          url
+        }
+      }
+      coverCaption
+      content {
+        json
+      }
+      keywords
+    }
+  }
+`
+
+BlogPost.propTypes = {
+  data: PropTypes.objectOf(PropTypes.object.isRequired),
+}
