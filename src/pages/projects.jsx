920e61eab4dba853803ec37e8382e2342ec52c47
Henrik Bjelland <henrikfb@icloud.com>
2021-05-25 13:22:10 +0200

    remove quote from project page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,27 +3,22 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
-import Blockquote from '../components/blockquote/Blockquote'
-import {HightlightedBlockquote} from '../components/blockquote/Blockquote'
 // Components
 import { Container } from '../components/container/Container'
 import { FullWidthContainer } from '../components/container/FullWidthContainer'
 import { Header } from '../components/header/Header'
 import Line from '../components/line/Line'
-import LinkButton from '../components/link-button/LinkButton'
 import SEO from '../components/seo/seo'
 import Layout from '../components/site-layout/Layout'
 import Statement from '../components/statement/Statement'
 import { StatementContent, StatementImage } from '../components/statement/Statement'
-import {TextBlock} from '../components/text-block/TextBlock'
+import { TextBlock } from '../components/text-block/TextBlock'
 import { H1, H3 } from '../components/typography/heading/Heading'
 // Mixins
 import { mediaQuery } from '../utils/mixins/mixMediaQuery'
-import { fontSize } from '../utils/tokens/tokenFontSize'
 
 const Projects = ({ data }) => {
   const textBlockList = data.contentfulTextBlockList.textBlocks;
-  const quotePosts = data.allContentfulQuote.edges
 
   return (
     <Layout>
@@ -64,18 +59,10 @@ const Projects = ({ data }) => {
           />
         </CustomStatementTwo>
 
-        <QuoteContainer>
-          {quotePosts.map(({ node: post }) => (
-            <Blockquote key={post.id} highlightedQuote={post.highlightedQuote} />
-          ))}
-        </QuoteContainer>
-
       </FullWidthContainer>
     </Layout>
   )
 }
-
-export default Projects
 
 export const pageQuery = graphql`
   query ProjectsQuery {
@@ -122,29 +109,7 @@ export const pageQuery = graphql`
     }
     reverseOrder
   }
-  allContentfulQuote(
-      filter: { contentful_id: { eq: "1Re1NDWZnyBFrSKuvxe2VE" } }
-    ) {
-      edges {
-        node {
-          id
-          title
-          highlightedQuote {
-            childMarkdownRemark {
-              html
-            }
-          }
-          content {
-            childMarkdownRemark {
-              html
-            }
-          }
-          cite
-          author
-        }
-      }
-    }
-  }
+}
 `
 
 Projects.propTypes = {
@@ -204,34 +169,4 @@ const CustomStatementTwo = styled(CustomStatement)`
   `};
 `;
 
-const QuoteContainer = styled.div`
-  grid-column: 1 / -1;
-  margin-bottom: 4rem;
-
-  ${HightlightedBlockquote} {
-    ${mediaQuery.BREAKPOINT_3`
-
-      p {
-        font-size:  ${fontSize.FONT_SIZE_4} !important;
-        padding-bottom: 27px;
-        :last-child {
-          padding-bottom: 0;
-        }
-      }
-  `};
-  }
-
-  ${mediaQuery.BREAKPOINT_3`
-    ${mediaQuery.BREAKPOINT_3`
-      margin-bottom: 100px;
-      margin-top: 6rem;
-    `};
-  `};
-`;
-
-const CenteredContainer = styled(Container)`
-  grid-column: 1 / -1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+export default Projects
